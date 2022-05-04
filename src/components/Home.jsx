b05412713584caed9a084328fe874ab1c0bca343
Abdirahman <abdialicanada20@gmail.com>
2022-05-04 15:26:20 -0400

    update color scheme

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-scroll';
 
  const Home = () => {
   return (
-   <div name='home' className='w-full h-screen bg-[#0a192f]'>
+   <div name='home' className='w-full h-screen bg-[#2e3243]'>
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-pink-600'>Hi, my name is</p>
+        <p className='text-orange-600'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
           Abdi Ali
         </h1>
@@ -18,7 +18,7 @@ import { Link } from 'react-scroll';
         </p>
         <div>
             <Link to='work' smooth={true} duration={500}>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-orange-600 hover:border-orange-600'>
             View Work
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3 ' />
